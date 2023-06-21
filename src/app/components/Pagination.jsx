de400baee58f16dd84e627592a4a555459cbc400
Fadhli <marvelouslyfadhli@gmail.com>
2023-06-22 03:44:03 +0800

    fixed issue where pagination will always lead to people's page

--- src/app/components/Pagination.jsx
+++ src/app/components/Pagination.jsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Pagination(params) {
-
     const currentpage = parseInt(params.currentpage);
     const [par, setPar] = useState("");
 
@@ -17,14 +16,11 @@ export default function Pagination(params) {
             setPar("upcoming");
         } else if (params.route == "search") {
             setPar("search");
-        }
-        else if (params.route == "tvshows") {
+        } else if (params.route == "tvshows") {
             setPar("tvshows");
+        } else if (params.route == "people") {
+            setPar("people");
         }
-        else if (params.route == "people") {
-
-        }setPar("people");
-        
     }, []);
 
     const next = () => {
@@ -33,13 +29,13 @@ export default function Pagination(params) {
         if (params.query) {
             // to cater for user search query
             if (params.genres) {
-                window.location.href = `/${par}?query=${
-                    encodeURIComponent(params.query)
-                }&genres=${genres}&page=${currentpage + 1}`;
+                window.location.href = `/${par}?query=${encodeURIComponent(
+                    params.query
+                )}&genres=${genres}&page=${currentpage + 1}`;
             } else {
-                window.location.href = `/${par}?query=${encodeURIComponent(params.query)}&page=${
-                    currentpage + 1
-                }`;
+                window.location.href = `/${par}?query=${encodeURIComponent(
+                    params.query
+                )}&page=${currentpage + 1}`;
             }
         } else {
             if (params.genres) {
@@ -58,13 +54,13 @@ export default function Pagination(params) {
         if (params.query) {
             // to cater for user search query
             if (params.genres) {
-                window.location.href = `/${par}?query=${encodeURIComponent(params.query)}&genres=${
-                    params.genres
-                }&page=${currentpage - 1}`;
+                window.location.href = `/${par}?query=${encodeURIComponent(
+                    params.query
+                )}&genres=${params.genres}&page=${currentpage - 1}`;
             } else {
-                window.location.href = `/${par}?query=${encodeURIComponent(params.query)}&page=${
-                    currentpage - 1
-                }`;
+                window.location.href = `/${par}?query=${encodeURIComponent(
+                    params.query
+                )}&page=${currentpage - 1}`;
             }
         } else {
             if (params.genres) {
@@ -90,14 +86,9 @@ export default function Pagination(params) {
                     <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
                 </IconButton>
                 <Typography color="white" className="font-normal">
-                    Page{" "}
-                    <strong className="text-white">
-                        {currentpage}
-                    </strong>{" "}
+                    Page <strong className="text-white">{currentpage}</strong>{" "}
                     of{" "}
-                    <strong className="text-white">
-                        {params.totalpages}
-                    </strong>
+                    <strong className="text-white">{params.totalpages}</strong>
                 </Typography>
                 <IconButton
                     size="sm"

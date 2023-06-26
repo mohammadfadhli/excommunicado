e1d888ba4879f8df4b132dc76458016276062aae
Fadhli <marvelouslyfadhli@gmail.com>
2023-06-27 00:49:00 +0800

    added proper padding to crew list

--- src/app/components/Crew.jsx
+++ src/app/components/Crew.jsx
@@ -18,12 +18,12 @@ export default function Crew(params) {
 
     return (
         <>
-            { directors.length != 0 ? <><h1 className="font-bold mt-5">{directors.length > 1 ? "Directors" : "Director"}</h1>
-            <div className="">{directors.join(", ")}</div></> : "" }
-            { producers.length != 0 ? <><h1 className="font-bold mt-5">{producers.length > 1 ? "Producers" : "Producer"}</h1>
-            <div className="">{producers.join(", ")}</div></> : "" }
-            { writers.length != 0 ? <><h1 className="font-bold mt-5">{writers.length > 1 ? "Writers" : "Writer"}</h1>
-            <div className="">{writers.join(", ")}</div></> : "" }
+            { directors.length != 0 ? <><div className="my-5"><h1 className="font-bold">{directors.length > 1 ? "Directors" : "Director"}</h1>
+            <div className="">{directors.join(", ")}</div></div></> : "" }
+            { producers.length != 0 ? <><div className="my-5"><h1 className="font-bold">{producers.length > 1 ? "Producers" : "Producer"}</h1>
+            <div className="">{producers.join(", ")}</div></div></> : "" }
+            { writers.length != 0 ? <><div className="my-5"><h1 className="font-bold">{writers.length > 1 ? "Writers" : "Writer"}</h1>
+            <div className="">{writers.join(", ")}</div></div></> : "" }
         </>
     );
 }
